420536bd60f432bb7d4d180c6c919360f5a5ca06
sarah-e-brooks <156720978+sarah-e-brooks@users.noreply.github.com>
2024-03-10 18:31:54 -0700

    js file

---
+++ app.js
@@ -0,0 +1,24 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+// answer: if you don't want to reassign the variable PI you would use cont instead of var.
+// if you want to reassign the variable PI you would use let
+
+/* Write an ES2015 Version */
+let PI = 3.14
+PI = 42; // this would cuase an error
+
+/* ## **Quiz**
+
+- What is the difference between ***var*** and ***let***?
+let is used when block scoping and cannot be redeclared.
+
+- What is the difference between **var** and ***const***?
+const is block scoped and cannot be reassigned nor can it be redeclared.
+
+- What is the difference between **let** and **const**?
+let can be reassigned and const cannot
+
+- What is hoisting?
+declarations of functions, variables, and classes are moved to the top of their scope, before execution
+
+## */
